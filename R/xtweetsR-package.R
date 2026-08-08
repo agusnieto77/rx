cc3d3d6bed6872771c40fe5ea14057fd1a7f3b86
agusnieto77 <agustin.nieto77@gmail.com>
2026-08-07 22:21:23 -0300

    feat: add %||% null-coalescing operator

--- R/xtweetsR-package.R
+++ R/xtweetsR-package.R
@@ -7,3 +7,7 @@
 #' @keywords internal
 #' @importFrom utils packageVersion
 "_PACKAGE"
+
+# Null-coalescing operator: returns x if not NULL, otherwise y.
+# Used throughout the package as a safer alternative to nested if/is.null checks.
+`%||%` <- function(x, y) if (is.null(x)) y else x
